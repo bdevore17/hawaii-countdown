c0a25283dfb27968d2e2102e85a6d217d72c63fc
bdevore17 <bdevore17@gmail.com>
2016-11-27 12:27:08 -0500

    change scheduling and add html email

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const mailgun = require('mailgun-js')({
   apiKey: process.env.API_KEY,
   domain: process.env.DOMAIN
 });
+const email = require('./email');
+const tripDate = new Date(process.env.year, process.env.MONTH - 1, process.env.DAY, process.env.HOUR, 0, 0, 0);
+console.log(tripDate);
 
-const sched = schedule.scheduleJob('0 * * * * *', function() {
+const sched = schedule.scheduleJob('0 * '+ (process.env.HOUR - 1) +' * * *', function() {
 
   var i = 1;
+
+  var oneDay = 24 * 60 * 60 * 1000;
+  var currentDate = new Date();
+  var diffDays = Math.round(Math.abs((tripDate.getTime() - currentDate.getTime())/(oneDay)));
 
   while (true) {
 
@@ -18,7 +25,7 @@ const sched = schedule.scheduleJob('0 * * * * *', function() {
       from: 'Hawaii Countdown <countdown@' + process.env.DOMAIN + '>',
       to: toEmail,
       subject: 'Today\'s Hawaii Countdown!',
-      text: 'Testing some Mailgun awesomness!'
+      html: email.firstHalf + diffDays + email.secondHalf
     };
 
     mailgun.messages().send(data, function(error, body) {
